65d22f99e02a36feb96e1a6d29000e7e563d9f12
JiviteshJamuda <jivitesh2006@gmail.com>
2020-08-16 11:38:09 +0530

    Barter system will rule the economy

--- screens/login.js
+++ screens/login.js
@@ -6,7 +6,9 @@ import {
     TouchableOpacity, 
     ScrollView, 
     KeyboardAvoidingView,
+    Alert,
     TextInput, 
+    Modal,
 } from 'react-native';
 import db from "../config";
 import firebase from "firebase";
@@ -15,31 +17,45 @@ export default class Login extends React.Component {
     constructor(){
         super();
         this.state={
-            emailId:'',
-            password:'',
-            firstName:'',
-            lastName:'',
-            address:'',
-            contact:'',
-            confirmPassword:'',
-            isModalVisible:false,
-        };
+        emailId:'',
+        password:'',
+        firstName:'',
+        lastName:'',
+        address:'',
+        contact:'',
+        confirmPassword:'',
+        isModalVisible:'false'
+        }
     }
 
-    userSignUp = (emailId, password) =>{        
-        firebase.auth().createUserWithEmailAndPassword(emailId, password)
-        .then(()=>{    
+    userSignUp = (emailId, password,confirmPassword) =>{
+        if(password !== confirmPassword){
+            return Alert.alert("password doesn't match\nCheck your password.")
+        }else{
+          firebase.auth().createUserWithEmailAndPassword(emailId, password)
+          .then(()=>{
+            db.collection('users').add({
+              first_name:this.state.firstName,
+              last_name:this.state.lastName,
+              contact:this.state.contact,
+              email_id:this.state.emailId,
+              address:this.state.address
+            })
             return alert(
-                'User Added Successfully',
-            );
-        })
-        .catch((error)=> {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        return alert(errorMessage)
-        });
-        
+                 'User Added Successfully',
+                 '',
+                 [
+                   {text: 'OK', onPress: () => this.setState({"isModalVisible" : false})},
+                 ]
+             );
+          })
+          .catch((error)=> {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            return Alert.alert(errorMessage)
+          });
+        }
     }
 
     userLogin = (emailId, password)=>{
@@ -54,9 +70,117 @@ export default class Login extends React.Component {
         })
     }
 
+    showModal = ()=>{
+        return(
+        <Modal
+          animationType="fade"
+          transparent={true}
+          visible={this.state.isModalVisible}
+          >
+          <View style={styles.modalContainer}>
+            <ScrollView style={{width:'100%'}}>
+              <KeyboardAvoidingView style={styles.KeyboardAvoidingView}>
+              <Text
+                style={styles.modalTitle}
+                >Registration</Text>
+              <TextInput
+                style={styles.formTextInput}
+                placeholder ={"First Name"}
+                maxLength ={8}
+                onChangeText={(text)=>{
+                  this.setState({
+                    firstName: text
+                  })
+                }}
+              />
+              <TextInput
+                style={styles.formTextInput}
+                placeholder ={"Last Name"}
+                maxLength ={8}
+                onChangeText={(text)=>{
+                  this.setState({
+                    lastName: text
+                  })
+                }}
+              />
+              <TextInput
+                style={styles.formTextInput}
+                placeholder ={"Contact"}
+                maxLength ={10}
+                keyboardType={'numeric'}
+                onChangeText={(text)=>{
+                  this.setState({
+                    contact: text
+                  })
+                }}
+              />
+              <TextInput
+                style={styles.formTextInput}
+                placeholder ={"Address"}
+                multiline = {true}
+                onChangeText={(text)=>{
+                  this.setState({
+                    address: text
+                  })
+                }}
+              />
+              <TextInput
+                style={styles.formTextInput}
+                placeholder ={"Email"}
+                keyboardType ={'email-address'}
+                onChangeText={(text)=>{
+                  this.setState({
+                    emailId: text
+                  })
+                }}
+              /><TextInput
+                style={styles.formTextInput}
+                placeholder ={"Password"}
+                secureTextEntry = {true}
+                onChangeText={(text)=>{
+                  this.setState({
+                    password: text
+                  })
+                }}
+              /><TextInput
+                style={styles.formTextInput}
+                placeholder ={"Confrim Password"}
+                secureTextEntry = {true}
+                onChangeText={(text)=>{
+                  this.setState({
+                    confirmPassword: text
+                  })
+                }}
+              />
+              <View style={styles.modalBackButton}>
+                <TouchableOpacity
+                  style={styles.registerButton}
+                  onPress={()=>
+                    this.userSignUp(this.state.emailId, this.state.password, this.state.confirmPassword)
+                  }
+                >
+                <Text style={styles.registerButtonText}>Register</Text>
+                </TouchableOpacity>
+              </View>
+              <View style={styles.modalBackButton}>
+                <TouchableOpacity
+                  style={styles.cancelButton}
+                  onPress={()=>this.setState({"isModalVisible":false})}
+                >
+                <Text style={{color:'#ff5722'}}>Cancel</Text>
+                </TouchableOpacity>
+              </View>
+              </KeyboardAvoidingView>
+            </ScrollView>
+          </View>
+        </Modal>
+      )
+    }
+
     render(){
         return(            
             <View style={{justifyContent: 'center',alignItems: 'center'}}>
+                {this.showModal()}
                 <Text style={styles.title}>Barter trades</Text>
                 <TextInput
                     style={styles.loginBox}
@@ -87,11 +211,10 @@ export default class Login extends React.Component {
                 </TouchableOpacity>
 
                 <TouchableOpacity
-                style={[styles.button,{marginBottom:20, marginTop:20}]}
-                onPress = {()=>{
-                    this.userSignUp(this.state.emailId, this.state.password);
-                }}>
-                <Text style={styles.buttonText}>Sign up</Text>
+                style={styles.button}
+                onPress={()=>this.setState({ isModalVisible:true})}
+                >
+                <Text style={styles.buttonText}>SignUp</Text>
                 </TouchableOpacity>
             </View>        
         )
